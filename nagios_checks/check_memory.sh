fc1efc2a108ae2c4731fc0ceb17f9f734ffc2691
mrz <root@localhost>
2017-12-01 12:04:48 +0100

    Ciao

--- nagios_checks/check_memory.sh
+++ nagios_checks/check_memory.sh
@@ -5,6 +5,8 @@
 # $2 is for critical
 warn=$1
 crit=$2
+[[ -z $1 ]] && echo "UNKNOWN- Please specify Warn and Crit thresholds" && exit 3
+[[ -z $2 ]] && echo "UNKNOWN- Please specify Warn and Crit thresholds" && exit 3
 
 # Collect memory statistics
 mem_stats=$(free | grep Mem)
@@ -16,15 +18,15 @@ mem_free=$(free | grep Mem | awk '{print $4}')
 output="Memory stats: $mem_stats | Total=$mem_total, User=$mem_used, Free=$mem_free"
 
 # Test values against thresholds
-if [ $mem_free -gt $warn ]
+if [[ $mem_free -gt $warn ]]
 then
     echo "OK- $output"
     exit 0
-elif [ $mem_free -le $warn -a $mem_free -gt $crit ]
+elif [[ $mem_free -le $warn ]] && [[ $mem_free -gt $crit ]]
 then
     echo "WARNING- $output"
     exit 1
-elif [ $mem_free -lt $crit ]
+elif [[ $mem_free -lt $crit ]]
 then
     echo "CRITICAL- $output"
     exit 2
