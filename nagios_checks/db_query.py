6e3607c46a0a63200b28da8d1cdf4c488cb13e91
mrz <root@localhost>
2017-12-05 10:43:14 +0100

    Added base64 decoding of query. (needed to bypass escaping of query)

--- nagios_checks/db_query.py
+++ nagios_checks/db_query.py
@@ -2,50 +2,55 @@ import MySQLdb
 import sys
 import argparse
 import ConfigParser
-
+import base64
 
 if __name__ == '__main__':
-        parser = argparse.ArgumentParser(description="Simple Nagios plugin to execute query on any MySQL database. \n\
-                                                                                                                                                                      Just provide DB information via command line parameters")
-        parser.add_argument("-H", "--host", help="Host where the DB is located", dest='host', required=False)
-        parser.add_argument("-P", "--port", help="Port of the DB Instance", dest="port", required=False)
+	parser = argparse.ArgumentParser(description="Simple Nagios plugin to execute query on any MySQL database.\n\Just provide DB information via command line parameters")
+	parser.add_argument("-H", "--host", help="Host where the DB is located", dest='host', required=False)
+	parser.add_argument("-P", "--port", help="Port of the DB Instance", type=int, dest="port", required=False)
 
-        parser.add_argument("-s", "--schema", help="DB schema", dest='schema', required=True)
-        parser.add_argument("-q", "--query", help="The actual query to execute", dest='query', required=True)
-        parser.add_argument("-u", "--user", help="User with rights to execute the query", dest='user', required=False)
-        parser.add_argument("-p", "--password", help="Password of the user", dest='password', required=False)
+	parser.add_argument("-s", "--schema", help="DB schema", dest='schema', required=True)
+	parser.add_argument("-q", "--query", help="The actual query to execute", dest='query', required=True)
+	parser.add_argument("-u", "--user", help="User with rights to execute the query", dest='user', required=False)
+	parser.add_argument("-p", "--password", help="Password of the user", dest='password', required=False)
+	parser.add_argument("-c", "--creds", help="Credentials file", dest='cred_file', required=False)
 
-        args = parser.parse_args()
 
-        host = args.host
-        if host is None:
-                host = '127.0.0.1'
+	# Parse received arguments
+	args = parser.parse_args()
 
-        port = args.port
-        if port is None:
-                port = 3306
-        port = int(port)
+	# Check if host was provided, otherwise set default value of localhost
+	host = args.host
+	if host is None:
+		host = '127.0.0.1'
 
-        schema = args.schema
-        query = args.query
+	# Check if port was provided, otherwise set default (3306)
+	port = args.port
+	if port is None:
+		port = 3306
 
-        user = args.user
-        password = args.password
-        if user is None and password is None:
-                sql_ini_fileparser = ConfigParser.RawConfigParser()
-                sql_ini_fileparser.read('/root/.my.cnf')
-                user = sql_ini_fileparser.get('client', 'user')
-                password = sql_ini_fileparser.get('client', 'password')
+	schema = args.schema
+	query = args.query
+	query = base64.b64decode(query)
 
-#       print "Query exec: Host=%s, Port=%s, User=%s, Password=%s, DB=%s" % (host, port, user, password, schema)
-        try:
-                db = MySQLdb.connect(host=host, port=port, user=user, passwd=password, db=schema)
-                cursor = db.cursor()
-                cursor.execute(query)
-                res = cursor.fetchall()
-                print "OK - %s" % str(res[0][0])
-                db.close()
-        except:
-                print "WARNING - Error executing query"
+	user = args.user
+	password = args.password
+	if user is None and password is None:
+		cred_file = args.cred_file
+		sql_ini_fileparser = ConfigParser.RawConfigParser()
+		sql_ini_fileparser.read(cred_file)
+		user = sql_ini_fileparser.get('client', 'user')
+		password = sql_ini_fileparser.get('client', 'password')
 
-        sys.exit(0)
+	#       print "Query exec: Host=%s, Port=%s, User=%s, Password=%s, DB=%s" % (host, port, user, password, schema)
+	try:
+		db = MySQLdb.connect(host=host, port=port, user=user, passwd=password, db=schema)
+		cursor = db.cursor()
+		cursor.execute(query)
+		res = cursor.fetchall()
+		print "OK - %s" % str(res[0][0])
+		db.close()
+	except:
+		print "WARNING - Error executing query"
+
+	sys.exit(0)
